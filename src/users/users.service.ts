9534df59c8c871c980fae7a78a9fa01cf35d2dcc
Rbitah <bsc-35-21@unima.ac.mw>
2025-06-16 21:45:44 +0200

    implimented forgot password and change/reset passowrd

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { RegisterDto } from 'src/authentication/dto/create-authentication.dto';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -25,7 +25,7 @@ export class UsersService {
 
   async loginUser(dto) {
     const user = await this.userRepository.findOne({ where: { email: dto.email } });
-
+     
     if (!user) {
       throw new NotFoundException("User Not Found")
     }
@@ -36,6 +36,33 @@ export class UsersService {
     }
     return user;
   }
+  async forgetPassword(email:string) {
 
+      if (typeof email !== 'string') {
+    throw new BadRequestException('Invalid email format');
+  }
+    
+    const emailL = String(email).trim().toLowerCase();
+const user = await this.userRepository.findOne({
+  where: { email: emailL },
+});
+
+    console.log(emailL)
+    if (!user) {
+      throw new NotFoundException("User Not found")
+    }
+    return user;
+  }
+
+  async changePasswordWithCode(email, newPassword) {
+    const user = await this.userRepository.findOne({ where: { email: email } });
+    if (!user) {
+      throw new NotFoundException("User Not found")
+    }
+    const password=newPassword;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.password = hashedPassword;
+    await this.userRepository.save(user);
+  }
 
 }
