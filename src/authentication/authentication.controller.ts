ba41ead7f670865c62e9150e3826b0e607ffe7f3
Rbitah <bsc-35-21@unima.ac.mw>
2025-06-16 18:16:25 +0200

    created signup and login endpoints

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,9 +1,19 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { LoginDto, RegisterDto } from './dto/create-authentication.dto';
 
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
+  @Post("signup")
+  signup(@Body() registerDto: RegisterDto) {
+    return this.authenticationService.register(registerDto);
+  }
+
+  @Post("login")
+  login(@Body() loginDto:LoginDto){
+    return this.authenticationService.login(loginDto)
+  }
 }
