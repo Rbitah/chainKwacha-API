1b40868f7c2aa858f48df2c5406b4b2b89ccc23e
Rbitah <bsc-35-21@unima.ac.mw>
2025-06-16 18:17:54 +0200

    implimented jwt accesstoken and sending 6 digit code to user using email

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -3,9 +3,18 @@ import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { EmailingService } from 'src/emailing/emailing.service';
 
-@Module({imports:[UsersModule],
+@Module({imports:[UsersModule,JwtModule.registerAsync({
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: { expiresIn:configService.get<string>('JWT_EXPIRATION') },
+    }),
+    inject: [ConfigService],
+  }),],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService,UsersService],
+  providers: [AuthenticationService,UsersService,EmailingService],
 })
 export class AuthenticationModule {}
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,19 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto, RegisterDto } from './dto/create-authentication.dto';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { EmailingService } from 'src/emailing/emailing.service';
 
 @Injectable()
 export class AuthenticationService {
 
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    private  emailingService:EmailingService
+  ) { }
 
   async register(dto: RegisterDto) {
     const user = await this.usersService.signUp(dto);
-    return { message: 'User Created' };
+    const token = await this.generateAccessToken(user.user_ID, user.userRole);
+    const code = await this.generateSixdigitCode();
+    const sendLoginCode = await this.emailingService.sendWelcomeVerificationEmail(user.email,code,user.username)
+    return { token };
   }
 
   async login(dto: LoginDto) {
     const user = await this.usersService.loginUser(dto);
-    return {message:'user logged in'}
+    const token = await this.generateAccessToken(user.user_ID, user.userRole);
+    const code = await this.generateSixdigitCode();
+
+    const sendLoginCode = await this.emailingService.send2FA_code_ViaEmail(user.email,code,user.username)
+    return {token}
   }
+
+  async generateAccessToken(user_ID: string, userRole: string[]): Promise<string> {
+    const accessToken = await this.jwtService.sign({ user_ID, userRole })
+    return accessToken;
+  }
+
+  async generateSixdigitCode():Promise<string> {
+    const code = Math.floor(100000 + Math.random() * 900000).toString();
+
+    return code;
+  }
+
 }
