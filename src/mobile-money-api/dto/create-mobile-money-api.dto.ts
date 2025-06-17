6b3da0427b291318d1ea1237e7959f9bf142dd7c
Rbitah <bsc-35-21@unima.ac.mw>
2025-06-17 15:25:58 +0200

    implimented dtos for airtelmoney service

--- src/mobile-money-api/dto/create-mobile-money-api.dto.ts
+++ src/mobile-money-api/dto/create-mobile-money-api.dto.ts
@@ -1,1 +1,72 @@
-export class CreateMobileMoneyApiDto {}
+export class CreateMobileMoneyApiDto { }
+
+export interface OAuthTokenResponse {
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+}
+
+export interface SubscriberDto {
+  country: string;
+  currency?: string;
+  msisdn: string;
+}
+
+export interface TransactionDto {
+  amount: number;
+  country?: string;
+  currency?: string;
+  id: string;
+}
+
+export interface PaymentRequestDto {
+  reference: string;
+  subscriber: SubscriberDto;
+  transaction: TransactionDto;
+}
+
+export interface RefundRequestDto {
+  airtel_money_id: string;
+}
+
+
+export interface SubscriberDto {
+  country: string;
+  currency?: string;
+  msisdn: string;
+}
+
+export interface TransactionDto {
+  amount: number;
+  country?: string;
+  currency?: string;
+  id: string;
+}
+
+export interface PaymentRequestDto {
+  reference: string;
+  subscriber: SubscriberDto;
+  transaction: TransactionDto;
+}
+
+export interface RefundRequestDto {
+  airtel_money_id: string;
+}
+
+export interface DisbursementPayeeDto {
+  msisdn: string;
+  wallet_type: 'NORMAL';
+}
+
+export interface DisbursementTransactionDto {
+  amount: number;
+  id: string;
+  type: 'B2C' | 'B2B';
+}
+
+export interface DisbursementRequestDto {
+  payee: DisbursementPayeeDto;
+  reference: string;
+  pin: string;
+  transaction: DisbursementTransactionDto;
+}
